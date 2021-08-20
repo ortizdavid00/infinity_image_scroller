3f9d8012b51d077b508dfa7a20261e936a873596
David Ortiz-Alejandre <73569286+ortizdavid00@users.noreply.github.com>
2021-08-20 16:50:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+import FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
+
+FingerprintJS.load({token: '8uso7PORnHtbwMig7dH2', endpoint: 'https://fp.david-infinity-scroller.com'})
+  .then(fp => fp.get())
+  .then(result => console.log(result.visitorId))
+
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
